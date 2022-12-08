254f1a28a088aa6cb04b7f888c42c441e7dae4fc
mohammad hosein mousavi <biomousavi@gmail.com>
2022-12-08 20:22:41 +0330

    add showcase cards

---
+++ client/src/components/ShowcaseCard.vue
@@ -0,0 +1,197 @@
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+
+const card = ref<HTMLDivElement | null>(null);
+const width = ref(0);
+const height = ref(0);
+const mouseX = ref(0);
+const mouseY = ref(0);
+const mouseLeaveDelay = ref<number | null>(null);
+
+const mousePX = computed(() => mouseX.value / width.value);
+const mousePY = computed(() => mouseY.value / height.value);
+const cardStyle = computed(() => {
+  const rX = mousePX.value * 30;
+  const rY = mousePY.value * -30;
+  return {
+    transform: `rotateY(${rX}deg) rotateX(${rY}deg)`,
+  };
+});
+
+const cardBgTransform = computed(() => {
+  const tX = mousePX.value * -40;
+  const tY = mousePY.value * -40;
+  return {
+    transform: `translateX(${tX}px) translateY(${tY}px)`,
+  };
+});
+
+function handleMouseMove(e: MouseEvent) {
+  mouseX.value = e.pageX - card.value!.offsetLeft - width.value / 2;
+  mouseY.value = e.pageY - card.value!.offsetTop - height.value / 2;
+}
+
+function handleMouseEnter() {
+  clearTimeout(mouseLeaveDelay.value!);
+}
+
+function handleMouseLeave() {
+  mouseLeaveDelay.value = setTimeout(() => {
+    mouseX.value = 0;
+    mouseY.value = 0;
+  }, 600);
+}
+
+onMounted(() => {
+  width.value = card.value!.offsetWidth;
+  height.value = card.value!.offsetHeight;
+});
+</script>
+
+<template>
+  <div
+    ref="card"
+    class="card-wrap"
+    @mousemove="handleMouseMove"
+    @mouseenter="handleMouseEnter"
+    @mouseleave="handleMouseLeave"
+  >
+    <div
+      data-sal-duration="1000"
+      data-sal="slide-up"
+      data-sal-repeat
+      data-sal-delay="300"
+      data-sal-easing="ease-out-back"
+      class="card"
+      :style="cardStyle"
+    >
+      <div class="card-bg box-content" :style="[cardBgTransform]"></div>
+      <div class="card-info">
+        <slot name="header"></slot>
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+$hoverEasing: cubic-bezier(0.23, 1, 0.32, 1);
+$returnEasing: cubic-bezier(0.445, 0.05, 0.55, 0.95);
+
+p {
+  line-height: 1.5em;
+}
+
+h3 + p,
+p + p {
+  margin-top: 10px;
+}
+
+.card-wrap {
+  margin: 10px;
+  transform: perspective(800px);
+  transform-style: preserve-3d;
+  cursor: pointer;
+
+  &:hover {
+    .card-info {
+      transform: translateY(0);
+    }
+    .card-info p {
+      opacity: 1;
+    }
+    .card-info,
+    .card-info p {
+      transition: 0.6s $hoverEasing;
+    }
+    .card-info:after {
+      transition: 5s $hoverEasing;
+      opacity: 1;
+      transform: translateY(0);
+    }
+    .card-bg {
+      transition: 0.6s $hoverEasing, opacity 5s $hoverEasing;
+      opacity: 0.8;
+    }
+    .card {
+      transition: 0.6s $hoverEasing, box-shadow 2s $hoverEasing;
+      box-shadow: rgba(white, 0.2) 0 0 40px 5px, rgba(white, 1) 0 0 0 1px,
+        rgba(black, 0.66) 0 30px 60px 0, inset #333 0 0 0 5px,
+        inset white 0 0 0 6px;
+    }
+  }
+}
+
+.card {
+  position: relative;
+  flex: 0 0 240px;
+  width: 240px;
+  height: 320px;
+  background-color: #333;
+  overflow: hidden;
+  border-radius: 10px;
+  box-shadow: rgba(black, 0.66) 0 30px 60px 0, inset #333 0 0 0 5px,
+    inset rgba(white, 0.5) 0 0 0 6px;
+  transition: 1s $returnEasing;
+}
+
+.card-bg {
+  opacity: 0.5;
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  transition: 1s $returnEasing, opacity 5s 1s $returnEasing;
+  pointer-events: none;
+}
+
+.card-info {
+  padding: 20px;
+  position: absolute;
+  bottom: 0;
+  color: #fff;
+  transform: translateY(40%);
+  transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
+
+  p {
+    opacity: 0;
+    text-shadow: rgba(black, 1) 0 2px 3px;
+    transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+
+  * {
+    position: relative;
+    z-index: 1;
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(
+      to bottom,
+      transparent 0%,
+      rgb(143, 143, 143) 100%
+    );
+    background-blend-mode: overlay;
+    opacity: 0;
+    transform: translateY(100%);
+    transition: 5s 1s $returnEasing;
+  }
+}
+
+.card-info h3 {
+  font-size: 17px;
+  font-weight: 700;
+  text-shadow: rgba(black, 0.5) 0 10px 10px;
+}
+</style>
--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -1,34 +1,73 @@
-<!-- <script setup lang="ts"></script> -->
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const ShowcaseCard = defineAsyncComponent(
+  () => import("@/components/ShowcaseCard.vue")
+);
+
+const useCases = [
+  {
+    title: "Code with your team",
+    desc: "Open a Codeshare editor, write or copy the code, then share it with friends and colleagues. Pair program and troubleshoot together.",
+  },
+  {
+    title: "Interview developers",
+    desc: "Set coding tasks and observe in real-time when interviewing remotely or in person. Nobody likes writing code on a whiteboard.",
+  },
+  {
+    title: "Teach people to program",
+    desc: "Share your code with students and peers then educate them. Universities and colleges around the world use Codeshare every day.",
+  },
+];
+</script>
 
 <template>
-  <main
-    style="position: relative; min-height: 100vh"
-    class="d-flex flex-column align-center"
-  >
-    <h1 class="text-h3 font-weight-bold mt-12 quicksand">
-      Share the code in real-time
-    </h1>
+  <main>
+    <div
+      style="position: relative; min-height: 100vh"
+      class="d-flex flex-column align-center"
+    >
+      <div style="z-index: 1" class="d-flex flex-column align-center">
+        <h1 class="text-h3 font-weight-bold mt-12 quicksand">
+          Share the code in real-time
+        </h1>
 
-    <p class="my-5">A lightweight and real-time code editor in your browser.</p>
-    <v-btn color="black" class="font-weight-bold" rounded="lg">
-      Get started
-    </v-btn>
+        <p class="my-5">
+          A lightweight and real-time code editor in your browser.
+        </p>
+        <v-btn color="black" class="font-weight-bold" rounded="lg">
+          Get started
+        </v-btn>
+      </div>
 
-    <dotlottie-player
-      src="/FIVd9i7InI.lottie"
-      background="transparent"
-      speed="1"
-      style="
-        width: 100%;
-        height: 100vh;
-        position: absolute;
-        left: -6%;
-        bottom: -4%;
-      "
-      loop
-      autoplay
-    ></dotlottie-player>
+      <dotlottie-player
+        src="/FIVd9i7InI.lottie"
+        background="transparent"
+        speed="1"
+        style="
+          width: 100%;
+          height: 100vh;
+          position: absolute;
+          left: -6%;
+          bottom: -4%;
+          transform: scale(1.1);
+        "
+        loop
+        autoplay
+      ></dotlottie-player>
+    </div>
+
+    <div class="d-flex flex-wrap justify-space-around py-10 my-16">
+      <template v-for="useCase in useCases" :key="useCase.title">
+        <ShowcaseCard>
+          <template v-slot:header>
+            <h3 class="font-weight-bold">{{ useCase.title }}</h3>
+          </template>
+          <template v-slot:content>
+            <p>{{ useCase.desc }}</p>
+          </template>
+        </ShowcaseCard>
+      </template>
+    </div>
   </main>
-
-  <!--  -->
 </template>
